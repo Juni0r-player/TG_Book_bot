ac115bfbd0ff4e7e81e2c15ce2dcf0a13ad2aea7
Juni0r-player <vans-02@mail.ru>
2024-07-31 00:11:38 +0300

    Update other_handlers.py

---
+++ handlers/other_handlers.py
@@ -0,0 +1,12 @@
+# Настраивается после bookmarks_kb.py
+from aiogram import Router
+from aiogram.types import Message
+
+router = Router()
+
+
+# Этот хэндлер будет реагировать на любые сообщения пользователя,
+# не предусмотренные логикой работы бота
+@router.message()
+async def send_echo(message: Message):
+    await message.answer(f'Это эхо! {message.text}')
