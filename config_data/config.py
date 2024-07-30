20ee9ba7b35c3d236ef1aed19b0ff638a26caf94
Juni0r-player <vans-02@mail.ru>
2024-07-31 00:11:30 +0300

    Update config.py

--- config_data/config.py
+++ config_data/config.py
@@ -5,18 +5,21 @@
 загружать через config.py в нашего бота. Для этого мы имортируем дата классы,
 в которых будем хранить наши данные"""
 
+
 from dataclasses import dataclass
 from environs import Env
 
 # Создается датакласс для хранения токена и айди админа
 @dataclass
 class TgBot:
-    token: str # В данной строке будет храниться токен бота
-    admin_ids: str # В данной строке будет храниться айди админа
+    token: str  #  В данной строке будет храниться токен бота
+    admin_ids: list[int]  # В данной строке будет храниться айди админа
+
 
 @dataclass
 class Config:
     tg_bot: TgBot # Мы как бы создали базу бота, который будет тянуть данные из вышестоящего датакласса.
+
 
 # Создается функция, которая умеет читать секретный env файл
 # и возвращать экземпляр класса с подтянутыми данными оттуда
@@ -25,7 +28,7 @@ def load_config(path: str | None = None) -> Config:
     env.read_env(path) # прочитай вставленный файлик
     return Config( # Верни загруженного бота с таким то токеном и такими то админами
         tg_bot=TgBot(
-        env.read_env("BOT_TOKEN"), # Прочитай файл енв и вытащи токен бота и админов
-        admin_ids=list(map(int, env.list("ADMIN_IDS")))
+            token=env('BOT_TOKEN'), # Прочитай файл енв и вытащи токен бота и админов
+            admin_ids=list(map(int, env.list('ADMIN_IDS')))
+        )
     )
-)
